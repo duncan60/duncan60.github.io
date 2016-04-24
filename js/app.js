9cb8ed7f6201bd73cd9e4393a93b51b4a55d546b
duncan60 <tmduncan60@gmail.com>
2016-04-24 12:06:48 +0800

    push source code

---
+++ js/app.js
@@ -0,0 +1,97 @@
+﻿var $pageMenu,$wrap,$nav,$navPoint,$stAry,
+	pageId=["about","skills","works"],targetID="",pageStatus="";
+function isTransitionsSupported() {
+    var thisBody = document.body || document.documentElement,
+        thisStyle = thisBody.style,
+        support = thisStyle.transition !== undefined || 
+                  thisStyle.WebkitTransition !== undefined || 
+                  thisStyle.MozTransition !== undefined || 
+                  thisStyle.MsTransition !== undefined || 
+                  thisStyle.OTransition !== undefined;
+    return support;
+}
+
+var init=function(){
+	getDOM();
+	setEventHandler();
+}
+
+var getDOM=function(){
+	 $pageMenu=$("#page-menu");
+	 $wrap=$(".wrapper");
+	 $nav=$(".main-nav");
+	 $navPoint=$(".nav-opint");
+	 $stAry=[$("#about"),$("#skills"),$("#works")];
+}
+var setEventHandler=function(){
+	$navPoint.on('click',function(){
+		$pageMenu.toggleClass('open');
+		$navPoint.toggleClass('active');
+		$wrap.toggleClass('effect-back');
+		if($wrap.hasClass('effect-back')){
+		 	pageStatus="fadeOut";
+		 	//if($._data( $(".effect-back")[0], 'events' )==undefined){
+		 	$(".effect-back").off('transitionend webkitTransitionEnd oTransitionEnd MSTransitionEnd',effectBackComplete);
+		 	$(".effect-back").on('transitionend webkitTransitionEnd oTransitionEnd MSTransitionEnd',effectBackComplete);
+		 	//}
+		 }
+	})
+	$nav.on('click','a',navHandler).on('mouseenter mouseleave','a',function(e){
+		$("#"+$(this).attr('data-page')).toggleClass('up');
+	});
+}
+var navHandler=function(e){
+	
+	pageStatus="fadeIn";
+	targetID=$(this).attr('data-page');	
+	$.each(pageId, function( index, value ) {
+		if(value!=targetID  ){
+		  $stAry[index].addClass('fade-out');
+		 }
+	});
+	$(".fade-out").off('animationend webkitAnimationEnd oAnimationEnd MSAnimationEnd',stFadeOutComplete);
+	$(".fade-out").on('animationend webkitAnimationEnd oAnimationEnd MSAnimationEnd',stFadeOutComplete);
+	e.preventDefault();
+}
+var stFadeOutComplete=function(){
+	reIndex(targetID);
+	$pageMenu.removeClass('open');
+	$navPoint.removeClass('active');
+	$wrap.removeClass('effect-back');	
+}
+var effectBackComplete=function(){ 
+	ageStatus="fadeOut";
+	if(pageStatus=="fadeIn"){
+		$.each(pageId, function( index, value ) {	
+	  		$("#"+value).removeClass('fade-out');
+		});
+	}
+}
+
+var reIndex=function(target){
+	$.each(pageId, function( index, value ) {		 
+	  	$stAry[index].removeClass('first second third');
+	});
+	switch(target) {
+   		case "about":
+   			$stAry[0].addClass('first');
+       		$stAry[1].addClass('second');
+       		$stAry[2].addClass('third');
+        break;
+    	case "skills":
+        	$stAry[2].addClass('second');
+       		$stAry[0].addClass('third');
+       		$stAry[1].addClass('first');
+        break;
+   		case "works":
+   			$stAry[2].addClass('first');
+        	$stAry[0].addClass('second');
+       		$stAry[1].addClass('third');
+        break;
+        default:
+
+        break;
+	}
+	
+}
+init();
---
+++ js/ga_public.js
@@ -0,0 +1,40 @@
+﻿//=====================================================GA
+ var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', '---ga id--']);
+ _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+
+function addPageView(str){
+  _gaq.push(['_trackPageview',str]);
+  //console.log("str:"+str);
+}
+function addEvent(id,evt,str){
+  _gaq.push(['_trackEvent',id,evt,str]);
+  //console.log("evt:"+evt+"=====str:"+str);
+}
+
+
+
+// google Analytics ==============
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+ga('create', 'UA-44365926-1', 'muji.tw');
+
+// google Analytics END===========
+
+function trackpage(someurl){
+  ga('send', 'pageview', someurl);
+}
+
+function trackevent(tk){
+  ga('send', 'event', 'button', 'click', tk);
+}
