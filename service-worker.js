c96cb251e19491fe03f8156120577efc70e3342c
duncan60 <tmduncan60@gmail.com>
2018-01-04 16:48:26 +0800

    use webp images

--- service-worker.js
+++ service-worker.js
@@ -20,27 +20,46 @@ self.addEventListener('install', event => {
       )
   );
 });
-
 // Cache any new resources as they are fetched
+
 self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches
-      .match(event.request, { ignoreSearch: true })
-      .then(function(response) {
-        if (response) {
-          return response;
-        }
-        var requestToCache = event.request.clone();
-        return fetch(requestToCache).then(function(response) {
-          if (!response || response.status !== 200) {
+  if (/\.jpg$|.png$/.test(event.request.url)) {
+    var supportsWebp = false;
+    if (event.request.headers.has('accept')) {
+      supportsWebp = event.request.headers.get('accept').includes('webp');
+    }
+
+    if (supportsWebp) {
+      var req = event.request.clone();
+
+      var returnUrl = req.url.substr(0, req.url.lastIndexOf('.')) + '.webp';
+
+      event.respondWith(
+        fetch(returnUrl, {
+          mode: 'no-cors'
+        })
+      );
+    }
+  } else {
+    event.respondWith(
+      caches
+        .match(event.request, { ignoreSearch: true })
+        .then(function(response) {
+          if (response) {
             return response;
           }
-          var responseToCache = response.clone();
-          caches.open(cacheName).then(function(cache) {
-            cache.put(requestToCache, responseToCache);
+          var requestToCache = event.request.clone();
+          return fetch(requestToCache).then(function(response) {
+            if (!response || response.status !== 200) {
+              return response;
+            }
+            var responseToCache = response.clone();
+            caches.open(cacheName).then(function(cache) {
+              cache.put(requestToCache, responseToCache);
+            });
+            return response;
           });
-          return response;
-        });
-      })
-  );
+        })
+    );
+  }
 });
